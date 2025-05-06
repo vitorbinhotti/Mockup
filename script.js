76e31e4b765b34cfd4684e0da37a1d18d0d744ed
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-05-06 09:11:47 -0300

    add

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function abrirMenu() {
 function fecharMenu() {
     document.querySelector("nav").classList.remove("ativo");
 }
+
+function mostrarAba(abaId) {
+    // Oculta todas as abas
+    const abas = document.querySelectorAll('.conteudo-aba');
+    abas.forEach(div => div.style.display = 'none');
+  
+    // Remove a classe 'ativa' de todos os botões
+    const botoes = document.querySelectorAll('.aba');
+    botoes.forEach(botao => botao.classList.remove('ativa'));
+  
+    // Mostra a aba clicada
+    document.getElementById(abaId).style.display = 'block';
+  
+    // Adiciona a classe 'ativa' ao botão clicado
+    event.target.classList.add('ativa');
+  }
