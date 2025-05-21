ddb1d55f1d7e220b33fe47979048a9e7dbe46bbe
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-05-21 08:27:14 -0300

    update - quase finalizado

--- script.js
+++ script.js
@@ -7,17 +7,13 @@ function fecharMenu() {
 }
 
 function mostrarAba(abaId) {
-    // Oculta todas as abas
     const abas = document.querySelectorAll('.conteudo-aba');
     abas.forEach(div => div.style.display = 'none');
   
-    // Remove a classe 'ativa' de todos os botões
     const botoes = document.querySelectorAll('.aba');
     botoes.forEach(botao => botao.classList.remove('ativa'));
   
-    // Mostra a aba clicada
     document.getElementById(abaId).style.display = 'block';
   
-    // Adiciona a classe 'ativa' ao botão clicado
     event.target.classList.add('ativa');
   }
