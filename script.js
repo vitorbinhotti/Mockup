5554fc2d3a44fcd8a9486eae69fb6afc5e3734cb
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-05-26 11:14:48 -0300

    update - bugs consertados

--- script.js
+++ script.js
@@ -1,49 +1,73 @@
-function mostrarAba(abaId, botaoClicado) {
+function abrirMenu() {
+  document.querySelector("nav").classList.add("ativo");
+}
+
+function fecharMenu() {
+  document.querySelector("nav").classList.remove("ativo");
+}
+
+function mostrarAba(abaId) {
   const abas = document.querySelectorAll('.conteudo-aba');
   abas.forEach(div => div.style.display = 'none');
 
+
   const botoes = document.querySelectorAll('.aba');
+  botoes.forEach(botao => botao.classList.remove('ativa'));
+
+
+  document.getElementById(abaId).style.display = 'block';
+
+
+  event.target.classList.add('ativa');
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btnMarcarComoLido = document.querySelectorAll('.btn-alertas')[0];
+  const btnDesativarNotificacoes = document.querySelectorAll('.btn-alertas')[1];
+  let notificacoesAtivas = true; 
+
+  btnMarcarComoLido.addEventListener('click', function () {
+      const notificacoesImportantes = document.querySelectorAll('.com-noti');
+      notificacoesImportantes.forEach(function (noti) {
+          noti.classList.remove('com-noti');
+          noti.classList.add('sem-noti');
+
+          const img = noti.querySelector('img');
+          if (img) {
+              img.src = 'images/sino-sem-not.png';
+              img.classList.remove('img-alertas-noti');
+              img.classList.add('img-alertas');
+          }
+      });
+  });
+
+  btnDesativarNotificacoes.addEventListener('click', function () {
+      const todasNotificacoes = document.querySelectorAll('.notificacoes > div');
+
+      if (notificacoesAtivas) {
+          todasNotificacoes.forEach(noti => {
+              noti.style.display = 'none';
+          });
+          btnDesativarNotificacoes.innerHTML = 'Ativar notificações <img src="images/sino-sem-not.png">';
+      } else {
+          todasNotificacoes.forEach(noti => {
+              noti.style.display = 'flex'; 
+          });
+          btnDesativarNotificacoes.innerHTML = 'Desativar notificações <img src="images/desativar-noti.png">';
+      }
+
+      notificacoesAtivas = !notificacoesAtivas; 
+  });
+});
+
+function mostrarAbaRelatorio(abaId, botaoClicado) {
+  const abas = document.querySelectorAll('.conteudo-aba-relatorio');
+  abas.forEach(div => div.style.display = 'none');
+
+  const botoes = document.querySelectorAll('.tab-relatorio .aba');
   botoes.forEach(botao => botao.classList.remove('ativa'));
 
   document.getElementById(abaId).style.display = 'block';
   botaoClicado.classList.add('ativa');
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  const btnMarcarComoLido = document.querySelectorAll('.btn-alertas')[0];
-  const btnDesativarNotificacoes = document.querySelectorAll('.btn-alertas')[1];
-  let notificacoesAtivas = true;
-
-  btnMarcarComoLido.addEventListener('click', function () {
-    const notificacoesImportantes = document.querySelectorAll('.com-noti');
-    notificacoesImportantes.forEach(function (noti) {
-      noti.classList.remove('com-noti');
-      noti.classList.add('sem-noti');
-
-      const img = noti.querySelector('img');
-      if (img) {
-        img.src = 'images/sino-sem-not.png';
-        img.classList.remove('img-alertas-noti');
-        img.classList.add('img-alertas');
-      }
-    });
-  });
-
-  btnDesativarNotificacoes.addEventListener('click', function () {
-    const todasNotificacoes = document.querySelectorAll('.notificacoes > div');
-
-    if (notificacoesAtivas) {
-      todasNotificacoes.forEach(noti => {
-        noti.style.display = 'none';
-      });
-      btnDesativarNotificacoes.innerHTML = 'Ativar notificações <img src="images/sino-sem-not.png">';
-    } else {
-      todasNotificacoes.forEach(noti => {
-        noti.style.display = 'flex';
-      });
-      btnDesativarNotificacoes.innerHTML = 'Desativar notificações <img src="images/desativar-noti.png">';
-    }
-
-    notificacoesAtivas = !notificacoesAtivas;
-  });
-});
